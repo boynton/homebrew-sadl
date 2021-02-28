74ab54cd24782c147a7a132d797e5cb0a023f5e9
boynton <lee@boynton.com>
2021-02-28 10:46:43 -0800

    Brew formula update for sadl version v1.6.3

--- Formula/sadl.rb
+++ Formula/sadl.rb
@@ -5,25 +5,25 @@
 class Sadl < Formula
   desc "SADL is a general high level API description language for http-based services."
   homepage "https://github.com/boynton/sadl"
-  version "1.6.2"
+  version "1.6.3"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/boynton/sadl/releases/download/v1.6.2/sadl_1.6.2_macOS_x86_64.tar.gz"
-    sha256 "91e30ba10cee441af15c96ca7ecb0d7ec03b253b0b1579193190ae9614a754df"
+    url "https://github.com/boynton/sadl/releases/download/v1.6.3/sadl_1.6.3_macOS_x86_64.tar.gz"
+    sha256 "cd716ebe7fb406ef6b4486d8cefbcee7df82d109caa92007e69503595215a150"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/boynton/sadl/releases/download/v1.6.2/sadl_1.6.2_macOS_arm64.tar.gz"
-    sha256 "2934ef8a562bfba9ede69af043e5350fbae6ad881dc39a11280104ab9c0d4dd0"
+    url "https://github.com/boynton/sadl/releases/download/v1.6.3/sadl_1.6.3_macOS_arm64.tar.gz"
+    sha256 "b0bfbe7dc0d616741de502a4d1662967e8726b08136b95c67996431e64c8ab43"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/boynton/sadl/releases/download/v1.6.2/sadl_1.6.2_Linux_x86_64.tar.gz"
-    sha256 "7d6ff951c3c6cc452de8dde4bd67667ae6418fdde62c21ab103b7a7d8ae1fb38"
+    url "https://github.com/boynton/sadl/releases/download/v1.6.3/sadl_1.6.3_Linux_x86_64.tar.gz"
+    sha256 "084fd36382684f102bfc90e2cc0e4defc110f8ba44fa5cf18fc4ce8cbc1539fd"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/boynton/sadl/releases/download/v1.6.2/sadl_1.6.2_Linux_arm64.tar.gz"
-    sha256 "12007bf86f0f65f1d5fab1c72f32be626c293f04b0ab8a8e41aa3243dd411f80"
+    url "https://github.com/boynton/sadl/releases/download/v1.6.3/sadl_1.6.3_Linux_arm64.tar.gz"
+    sha256 "9138f8ad18a3202aeddc697f12ea3a862b7b02e7d04a5ce59d1f93ecec431f46"
   end
 
   depends_on "go"
