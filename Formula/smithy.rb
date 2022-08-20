5ab9ae571e58405e05c332b18306e0f2d6ffca07
boynton <lee@boynton.com>
2022-08-20 10:42:11 -0700

    Brew formula update for smithy version v0.4.0

--- Formula/smithy.rb
+++ Formula/smithy.rb
@@ -5,21 +5,21 @@
 class Smithy < Formula
   desc "A Go-based tool to work with Smithy API Specifications."
   homepage "https://github.com/boynton/smithy"
-  version "0.3.1"
+  version "0.4.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/boynton/smithy/releases/download/v0.3.1/smithy_0.3.1_macOS_arm64.tar.gz"
-      sha256 "3dcacea11ce86016b19b6348e543bf1c19018ee7e2256fe58cf6e126f7447cf4"
+      url "https://github.com/boynton/smithy/releases/download/v0.4.0/smithy_0.4.0_macOS_arm64.tar.gz"
+      sha256 "ffa2e652b61cc4885f7d8c3982e9518c75d7a03da8aa60dc6b1632a0f2538103"
 
       def install
         bin.install "smithy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/boynton/smithy/releases/download/v0.3.1/smithy_0.3.1_macOS_x86_64.tar.gz"
-      sha256 "140974460e2bbf909c593d962776e5d3533fb8df6469928a09a95cbe1a89d0e6"
+      url "https://github.com/boynton/smithy/releases/download/v0.4.0/smithy_0.4.0_macOS_x86_64.tar.gz"
+      sha256 "88422667f9133eb4bce4f1d57ba468d4c96ecfbac31ac2ba3a8bf0e47239330b"
 
       def install
         bin.install "smithy"
@@ -29,16 +29,16 @@ class Smithy < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/boynton/smithy/releases/download/v0.3.1/smithy_0.3.1_Linux_arm64.tar.gz"
-      sha256 "3c23df06688e18e0fcbaca2e82e5c39b50eb4a51f12d38543f5b64b397efba20"
+      url "https://github.com/boynton/smithy/releases/download/v0.4.0/smithy_0.4.0_Linux_arm64.tar.gz"
+      sha256 "8ae8beb7b9079cc3710109d1888d18f4ae8eef35212267dfcf25a5b2de46fb7a"
 
       def install
         bin.install "smithy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/boynton/smithy/releases/download/v0.3.1/smithy_0.3.1_Linux_x86_64.tar.gz"
-      sha256 "dbff77a877e12d0026c45a5dbcb6cbb27cbce5f1d87343806d11a0fa5869f2c8"
+      url "https://github.com/boynton/smithy/releases/download/v0.4.0/smithy_0.4.0_Linux_x86_64.tar.gz"
+      sha256 "b306bd09fc24ac7f903fd274d3398415d352a27c8fbece4025f8fc9c9ae65b63"
 
       def install
         bin.install "smithy"
