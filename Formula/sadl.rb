8d3714bb1b316724833ee10d79c290d09ac673a2
boynton <lee@boynton.com>
2022-10-03 20:34:23 -0700

    Brew formula update for sadl version v1.8.2

--- Formula/sadl.rb
+++ Formula/sadl.rb
@@ -5,21 +5,21 @@
 class Sadl < Formula
   desc "SADL is a general high level API description language for http-based services."
   homepage "https://github.com/boynton/sadl"
-  version "1.8.1"
+  version "1.8.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/boynton/sadl/releases/download/v1.8.1/sadl_1.8.1_macOS_arm64.tar.gz"
-      sha256 "98445de28b538a305723e6f4383cd3cda081465a1d0eb82668f3d507d1b9236a"
+      url "https://github.com/boynton/sadl/releases/download/v1.8.2/sadl_1.8.2_macOS_arm64.tar.gz"
+      sha256 "3d01ec1b08fd1580d066bb855009eec0a00e7fea35a3e4a463ecf95cea608705"
 
       def install
         bin.install "sadl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/boynton/sadl/releases/download/v1.8.1/sadl_1.8.1_macOS_x86_64.tar.gz"
-      sha256 "fcf416353759e6f440845f869c639cf1fa5acaa362c9a13475e61b59ac773ea9"
+      url "https://github.com/boynton/sadl/releases/download/v1.8.2/sadl_1.8.2_macOS_x86_64.tar.gz"
+      sha256 "ae25fcf361d7b10c4aab2d314aa99f16f93af1594cfbe4be2d70eb81f7d385fa"
 
       def install
         bin.install "sadl"
@@ -29,16 +29,16 @@ class Sadl < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/boynton/sadl/releases/download/v1.8.1/sadl_1.8.1_Linux_arm64.tar.gz"
-      sha256 "5e580336f49ab15a87c58c38387c5fadf866016b01133600cb275243ac35959d"
+      url "https://github.com/boynton/sadl/releases/download/v1.8.2/sadl_1.8.2_Linux_arm64.tar.gz"
+      sha256 "21cf91bf1daeb453b257f47b896dc5fe0c6960d629cf03882c0f7d6a956be269"
 
       def install
         bin.install "sadl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/boynton/sadl/releases/download/v1.8.1/sadl_1.8.1_Linux_x86_64.tar.gz"
-      sha256 "c68ead8155d31c1d44d9e377fccbf54c3e0eeb95f1f2e2c9785bf34fd7b163e8"
+      url "https://github.com/boynton/sadl/releases/download/v1.8.2/sadl_1.8.2_Linux_x86_64.tar.gz"
+      sha256 "a81f101ec8237e2442510ab97e24fcc501c7dea5e80fbd0e39c1c8c45d0c5b27"
 
       def install
         bin.install "sadl"
