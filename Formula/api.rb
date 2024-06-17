565f3f34099ca2cb1a2066daf5c1a6c0687c14a8
boynton <lee@boynton.com>
2024-06-17 14:56:45 -0700

    Brew formula update for api version v0.3.7

--- Formula/api.rb
+++ Formula/api.rb
@@ -5,23 +5,23 @@
 class Api < Formula
   desc "A command line tool to work with API Specifications and code generation."
   homepage "https://github.com/boynton/api"
-  version "0.3.6"
+  version "0.3.7"
   license "Apache-2.0"
 
   depends_on "go"
 
   on_macos do
     on_intel do
-      url "https://github.com/boynton/api/releases/download/v0.3.6/api_darwin_amd64.tar.gz"
-      sha256 "9a71febe3c1cf055fc6c540454800e65c034d7ef8a37988db89e053053e1e708"
+      url "https://github.com/boynton/api/releases/download/v0.3.7/api_darwin_amd64.tar.gz"
+      sha256 "de04ce3ee2043ffcd3f71dc135db7c9d74886746cf8beb540d9acd59d1dcfb7c"
 
       def install
         bin.install "api"
       end
     end
     on_arm do
-      url "https://github.com/boynton/api/releases/download/v0.3.6/api_darwin_arm64.tar.gz"
-      sha256 "cbc265d335849d37bbbd9631122709fa8f83839e94d27a04bbe0492579b4eb74"
+      url "https://github.com/boynton/api/releases/download/v0.3.7/api_darwin_arm64.tar.gz"
+      sha256 "8324244c45d28fce75381522976a810f6d9f55e9958b95944f8dfd59b385e252"
 
       def install
         bin.install "api"
@@ -32,8 +32,8 @@ class Api < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/boynton/api/releases/download/v0.3.6/api_linux_amd64.tar.gz"
-        sha256 "b4ea080f1d538ab92f6841ec9b21fd1e5d4d26714c2906b5a780b36faec8e4f5"
+        url "https://github.com/boynton/api/releases/download/v0.3.7/api_linux_amd64.tar.gz"
+        sha256 "847faca694ca800f4774d924f6637e4b851e06d43b2dba495893502c4652f598"
 
         def install
           bin.install "api"
@@ -42,8 +42,8 @@ class Api < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/boynton/api/releases/download/v0.3.6/api_linux_arm64.tar.gz"
-        sha256 "0d867d846c407abb7f574af2bf814c16f4333b3ab7c03d26ffc5d9a5f9be92cc"
+        url "https://github.com/boynton/api/releases/download/v0.3.7/api_linux_arm64.tar.gz"
+        sha256 "6d8b468513b820660ab3eb38ee85b21dcc1324a0f1e3538110315ed601e26ac2"
 
         def install
           bin.install "api"
